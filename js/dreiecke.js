53cd0eea664d7be4e7507633bccd9f79ddfbd8bd
Demhaty <demhat.yasin@outlook.com>
2022-01-31 19:40:50 +0100

    Zwei_Seiten_und_eingeschlossene_Winkel

--- js/dreiecke.js
+++ js/dreiecke.js
@@ -5,46 +5,48 @@ var Dreieck = function(a,b,c,weg){
     //  a , b und c vorhanden
     //
     if(weg == 1){
-    this.a = a;
-    this.b = b;
-    this.c = c;
-    //
-    // alpha ,beta und gamma berechnen
-    //
-    this.alpha = acos((a * a - b * b - c * c) / (-2 * b * c));
-    this.beta = acos((b * b - c * c - a * a) / (-2 * c * a));
-    this.gamma = acos((c * c - a * a - b * b) / (-2 * a * b));
-    }
-    //
-    //  alpha , beta und gamma vorhanden
-    //
-    else if(weg == 2){
+        this.a = a;
+        this.b = b;
+        this.c = c;
         //
         // alpha ,beta und gamma berechnen
         //
-        this.alpha = a;
-        this.beta = b;
+        this.alpha = acos((a * a - b * b - c * c) / (-2 * b * c));
+        this.beta = acos((b * b - c * c - a * a) / (-2 * c * a));
+        this.gamma = acos((c * c - a * a - b * b) / (-2 * a * b));
+    }
+    //
+    //  a , b und gamma vorhanden
+    //
+    else if(weg == 2){
+        this.a = a;
+        this.b = b;
         this.gamma = c;
         //
-        //  a , b und c vorhanden
+        // c , alpha und beta berechnen
         //
-        a =  b * sin(alpha) / sin(beta);
-        b =
-        c = 
+        
+        this.c = Math.sqrt((a*a + b*b -2*a*b*cos(this.gamma)));
+        c=this.c;
+        this.alpha = acos((a * a - b * b - c * c) / (-2 * b * c));
+        this.beta = acos((b * b - c * c - a * a) / (-2 * c * a));
+
+
+        
     }
     // ha ,hb und hc berechnen
-    this.ha = b * sin(this.gamma);
-    this.hb = c * sin(this.alpha);
-    this.hc = a * sin(this.beta);
+    this.ha = this.b * sin(this.gamma);
+    this.hb = this.c * sin(this.alpha);
+    this.hc = this.a * sin(this.beta);
      
     //u 
-    this.u = a + b + c;
+    this.u = this.a + this.b + this.c;
     //A
-    this.A = this.ha * a / 2 ;
+    this.A = this.ha * this.a / 2 ;
     // ru
-    this.ru= a /(2 * sin(this.alpha));
+    this.ru= this.a /(2 * sin(this.alpha));
     // ri
-    this.ri = c * sin(this.alpha / 2) * sin(this.beta / 2) / sin((this.alpha + this.beta) / 2);
+    this.ri = this.c * sin(this.alpha / 2) * sin(this.beta / 2) / sin((this.alpha + this.beta) / 2);
     // sa
 
     
@@ -64,7 +66,11 @@ function atn(x)
 function sin(x){
     return Math.sin(x * Math.PI / 180);
 }
+function cos(x){
+    return Math.cos( x * Math.PI / 180);
+}
 
+//let a=0,b=0,c=0,alpha=0.0,beta=0.0,gamma=0.0;
 let a = document.getElementById('a').value;
 let b = document.getElementById('b').value;
 let c = document.getElementById('c').value;
@@ -74,8 +80,46 @@ if(a != 0 && b!=0 && c!=0){
     var new_dreick = new Dreieck(a,b,c,weg);
     console.log(new_dreick);
 }
-else if((a + b + c) == 180){
+//
+//  
+//
+/*
+else if((alpha + beta + gamma) == 180){
     let weg = 2;
     var new_dreick = new Dreieck(45,45,90,weg);
     console.log(new_dreick);
 }
+*/
+//
+// zwei Seiten und der eingeschlossene Winkel
+//new Dreieck(a,b,gamma,weg=2)
+if((a !=0 && b !=0 && gamma !=0)){
+let weg = 2;
+var new_dreick = new Dreieck(4,5,80,weg);
+console.log(new_dreick);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
